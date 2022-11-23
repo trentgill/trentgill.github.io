cca9d071f1d41aa4fd222ba917651f3e471dfa35
trentgill <trent.gill@gmail.com>
2022-11-22 21:20:54 -0500

    sync!

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
-cat head.htm_ > index.html  # copy header 
-cmark index.md >> index.html # convert md to html & append
-cat foot.htm_ >> index.html # append footer to index.html
+echo ">> root .md to .html"
+
+list=$(ls -r ./*.md)
+for file in $list ; do
+  date=$(date -r ${file} +%D)
+  file=${file:2}
+  file=${file%.*}
+  echo "$file"
+  target=${file}.html
+  cat head.htm_ > ${target}
+  cmark --unsafe ${file}.md >> ${target}
+  cat foot.htm_ >> ${target}
+  sed -i '' -e 's#DATE#'$date'#g' ${target}
+done
